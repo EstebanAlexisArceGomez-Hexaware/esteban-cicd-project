f9bf8b65777d839b1bba6a36291a96dfb319c35a
Esteban alexis Arce gomez <2000081765@hexaware.com>
2022-11-03 13:23:24 -0600

    * Change front

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <img src="https://th.bing.com/th/id/R.fe505e267661ada7df0990c7015d04fc?rik=mSmnDfUT2dDHgA&riu=http%3a%2f%2fimg03.deviantart.net%2fdd90%2fi%2f2016%2f363%2ff%2f1%2fgoku_ssj2_v2___dbkai___render_by_fradayesmarkers-dataoid.png&ehk=o4wC0GQevHbkllKMFhl6QnM9WsaV9Gm62OtSRq08ZsU%3d&risl=&pid=ImgRaw&r=0" alt="" />
+          <strong>Esteban Alexis QA</strong>
         </p>
         <a
           className="App-link"
