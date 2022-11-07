faedfd7cb6e0029f445203e67488d8ef318c47bd
Esteban alexis Arce gomez <2000081765@hexaware.com>
2022-11-07 10:46:38 -0600

    Fix workflow for QA

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img width={250} height={250} src="https://th.bing.com/th/id/R.fe505e267661ada7df0990c7015d04fc?rik=mSmnDfUT2dDHgA&riu=http%3a%2f%2fimg03.deviantart.net%2fdd90%2fi%2f2016%2f363%2ff%2f1%2fgoku_ssj2_v2___dbkai___render_by_fradayesmarkers-dataoid.png&ehk=o4wC0GQevHbkllKMFhl6QnM9WsaV9Gm62OtSRq08ZsU%3d&risl=&pid=ImgRaw&r=0" alt="" />
         <p>
-          <strong>Esteban Alexis :-)</strong>
+          <strong>Esteban Alexis :-(</strong>
         </p>
         <a
           className="App-link"
